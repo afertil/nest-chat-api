64cf50d9d9f4152e3f0f380d8be8ef0d6d22c39e
Anthony Fertil <anthony.fertil@hotmail.fr>
2018-02-01 23:03:50 +0100

    Manage user disconnection

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -5,6 +5,7 @@ import {
   WebSocketServer,
   WsException,
   OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import * as WebSocket from 'ws';
 import { Observable } from 'rxjs/Observable';
@@ -15,7 +16,7 @@ import { JwtService } from '../auth/jwt/jwt.service';
 import { User } from '../users/interfaces/user.interface';
 
 @WebSocketGateway({ port: 1080, namespace: 'rooms' })
-export class ChatGateway implements OnGatewayConnection {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server;
 
   constructor(private jwtService: JwtService) {}
@@ -26,42 +27,47 @@ export class ChatGateway implements OnGatewayConnection {
       true,
     );
 
-    console.log(socket.handshake.query.room);
     socket.join(socket.handshake.query.room);
 
     socket.broadcast.emit('userConnected', user);
   }
 
+  async handleDisconnect(socket) {
+    const room = socket.handshake.query.room;
+    socket.leave(room);
+  }
+
   @SubscribeMessage('message')
   onMessage(client, data): Observable<WsResponse<string>> {
+    const room = client.handshake.query.room;
     const event = 'message';
-    console.log(data);
 
-    client.broadcast.emit('newMessage', data);
+    client.broadcast.to(room).emit('message', data);
 
     return Observable.create(observer => observer.next({ event, data }));
   }
 
-  @SubscribeMessage('messages/join/:id')
+  @SubscribeMessage('join')
   onRoomConnection(client, data): void {
     const room = client.handshake.query.room;
 
     client.join(room);
   }
 
-  @SubscribeMessage('messages/leave/:id')
+  @SubscribeMessage('leave')
   onRoomLeave(client, data): void {
     const room = client.handshake.query.room;
-
+console.log('leave', room);
     client.leave(room);
   }
 
-  @SubscribeMessage('messages/:id')
+  @SubscribeMessage('messages')
   onMessageRoom(client, data): Observable<WsResponse<string>> {
     const room = client.handshake.query.room;
     const event = `message/${client.room.id}`;
     console.log(data);
-
+    console.log('--------');
+    console.log('Room', room);
     client.to(room).emit(data);
 
     return Observable.create(observer => observer.next({ event, data }));
