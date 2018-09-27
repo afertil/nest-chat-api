20916b384d19b543e0a92dcfb9f3eb1d326f4036
Anthony Fertil <anthony.fertil@forcity.io>
2018-09-27 13:51:15 +0200

    Remove old file + remove cors dep + add new deps for security

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import * as bodyParser from 'body-parser';
 import * as compression from 'compression';
-import * as cors from 'cors';
+import * as rateLimit from 'express-rate-limit';
 import * as express from 'express';
 import * as helmet from 'helmet';
 
@@ -9,24 +9,25 @@ import { ApplicationModule } from './modules/app.module';
 
 const app = express();
 
-app
-  .options('*', cors())
-  .use(cors())
-  .use(helmet())
+app.use(helmet())
   .use(compression())
   .use(bodyParser.json())
   .use(
     bodyParser.urlencoded({
       extended: true
     })
-  );
-// .use('/images', express.static(`${__dirname}/../uploads/images`));
+  )
+  .use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  }));
 
 async function bootstrap() {
   const nestApp = await NestFactory.create(ApplicationModule, app);
 
   // Add a route prefix
   nestApp.setGlobalPrefix('api');
+  nestApp.enableCors();
 
   await nestApp.listen(3000);
 }
