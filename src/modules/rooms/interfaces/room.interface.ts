82de33561663d71e3ce3eea3bdc9c1c388cf071e
Anthony Fertil <anthony.fertil@forcity.io>
2018-10-10 14:08:42 +0200

    Updates schema + interface to be able to manage private rooms and authorized users

--- src/modules/rooms/interfaces/room.interface.ts
+++ src/modules/rooms/interfaces/room.interface.ts
@@ -1,10 +1,13 @@
 import { Document } from 'mongoose';
 import { Message } from './message.interface';
+import { User } from '../../users/interfaces/user.interface';
 
 export interface Room extends Document {
   name: String;
   description?: String;
   is_user: Boolean;
+  is_private: Boolean;
+  users?: User[];
   messages?: Message[];
   created_at: Date;
   updated_at: Date;
--- src/modules/rooms/schemas/room.schema.ts
+++ src/modules/rooms/schemas/room.schema.ts
@@ -1,13 +1,16 @@
 import { Schema } from 'mongoose';
 import { MessageSchema } from './message.schema';
+import { UserSchema } from '../../users/schemas/user.schema';
 
 const room = new Schema({
   name: { type: String, required: true },
   description: { type: String },
   is_user: { type: Boolean, default: false },
+  is_private: { type: Boolean, default: false },
+  users: [UserSchema],
   messages: [MessageSchema],
   created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now }
 });
 
 /**
